bd7cf9b8365fa9c5720698df3c393cb5d2fe4528
apazhetam <techx.paz@gmail.com>
2020-08-15 20:09:14 -0400

    Removed OpenNMT files and updated website

--- mt.py
+++ mt.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-sys.path.insert(0, os.path.abspath("c:/users/foliage/downloads/opennmt-gui-master/venv/lib/site-packages"))
+path_to_packages = os.path.abspath(os.path.join("venv/lib/site-packages"))
+sys.path.insert(0, path_to_packages)
 
 from flask import Flask, render_template, jsonify, request
 from flask_wtf import FlaskForm
@@ -12,62 +13,76 @@ from wtforms.fields import SubmitField
 import requests
 import json
 import re
+from flask import Flask, jsonify, request
+from waitress import serve
+from onmt.translate import TranslationServer, ServerModelError
+import logging
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 pagedown = PageDown(app)
-
+STATUS_OK = "ok"
+STATUS_ERROR = "error"
+config_file = "available_models/conf.json"
+translation_server = TranslationServer()
+translation_server.start(config_file)
 
 class PageDownFormExample(FlaskForm):
-    pagedown = PageDownField('Type the text you want to translate and click "Translate".')
+    pagedown = PageDownField('')
     submit = SubmitField('Translate')
 
+def translate(inputs):
+    # inputs = request.get_json(force=True)
+    debug = False
+    out = {}
+    try:
+        trans, scores, n_best, _, aligns = translation_server.run(inputs)
+        assert len(trans) == len(inputs) * n_best
+        assert len(scores) == len(inputs) * n_best
+        assert len(aligns) == len(inputs) * n_best
+
+        out = [[] for _ in range(n_best)]
+        for i in range(len(trans)):
+            response = {"src": inputs[i // n_best]['src'], "tgt": trans[i],
+                        "n_best": n_best, "pred_score": scores[i]}
+            if len(aligns[i]) > 0 and aligns[i][0] is not None:
+                response["align"] = aligns[i]
+            out[i % n_best].append(response)
+    except ServerModelError as e:
+        model_id = inputs[0].get("id")
+        translation_server.models[model_id].unload()
+        out['error'] = str(e)
+        out['status'] = STATUS_ERROR
+    return out
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = PageDownFormExample()
     text = None
-    output = None
     language = "en-fr" #Default
     if form.validate_on_submit():
         source = form.pagedown.data
-        # source = re.sub(r"([?.!,:;¿])", r" \1 ", source)
-        # source = re.sub(r'[" "]+', " ", source)
-
-        print(source)
-
-        # arglength = len(sys.argv)
-
-        # if arglength <= 2:
-        ruleid = str(100)
-        # else:
-        #     ruleid = sys.argv[2]
-
-        # req = sys.argv[1]
-
-        url = "http://127.0.0.1:5000/translator/translate"
-        data='[{"src": "' + source +'", "id": ' + ruleid +'}]'
-        # data = [{"src": source, "id": 100}]
-        output = requests.post(url, data=data).text
-
-        myjson = json.loads(output)
-
-        # with open(output, "r") as read_file:
-        #    myjson = json.load(read_file)
-
-        input = myjson[0][0]['src']
-        output = myjson[0][0]['tgt']
-
-        print(input)
-        print(output)
-
-
-        text = jsn[0][0]['tgt']
-        # text = "hi"
+        source = re.sub(r"([?.!,:;¿])", r" \1 ", source)
+        source = re.sub(r'[" "]+', " ", source)
+        language = str(request.form.get('lang'))
+        # if language == "en-fr":
+        # url = "http://127.0.0.1:5000/translator/translate"
+        # elif language == "fr-en":
+        #     url = "http://127.0.0.1:3000/translator/translate"
+        # headers = {"Content-Type": "application/json"}
+        data = [{"src": source, "id": 100}]
+        # response = requests.post(url, json=data, headers=headers)
+        response = translate(data)
+        t = type(response)
+        # translation = response.text
+        # jsn = json.loads(response)
+        # text = jsn[0][0]['tgt']
+        text = response[0][0]['tgt']
         text = re.sub(r" ([?.!,:،؛؟¿])", r"\1", text)
     else:
-        form.pagedown.data = ('This is a very simple test.')
-    return render_template('index.html', form=form, language=language, text=output)
+        form.pagedown.data = ('namaste .')
+    return render_template('index.html', form=form, language=language, text=text)
 
 
 
